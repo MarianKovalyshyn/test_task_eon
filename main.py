2c8af896cfcfab921ec5e68e83461a213b18f3b3
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2024-01-31 02:03:32 +0200

    Add basic check-list

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` command
     """
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = (types.KeyboardButton(text=location) for location in locations)
     await message.answer("Привіт! Почнімо працювати.")
     await message.answer(
         text="Обери одну з п'яти локацій!",
-        reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add(
-            *(types.KeyboardButton(text=location) for location in locations)
-        ),
+        reply_markup=keyboard.add(*buttons),
     )
 
 
@@ -36,6 +36,48 @@ async def save_location(message: types.Message):
     """
     location = message.text
     await message.reply(f"Твою локацію було збережено!")
+    await send_checkbox(message)
+
+
+checkbox_states = {
+    option: False for option in ("Option 1", "Option 2", "Option 3")
+}
+
+
+async def send_checkbox(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+
+    for option, checked in checkbox_states.items():
+        text = f"✅ {option}" if checked else option
+        button = types.InlineKeyboardButton(
+            text=text, callback_data=f"checkbox_{option}"
+        )
+        keyboard.add(button)
+
+    button = types.InlineKeyboardButton(
+        text="Send", callback_data="checkbox_Send"
+    )
+    keyboard.add(button)
+    await message.answer(text="Select options:", reply_markup=keyboard)
+
+
+@dp.callback_query_handler(lambda query: query.data.startswith("checkbox_"))
+async def process_checkbox_callback(callback_query: types.CallbackQuery):
+    selected_option = callback_query.data.split("_")[1]
+
+    if selected_option == "Send":
+        checked_options = [
+            option for option, checked in checkbox_states.items() if checked
+        ]
+        await callback_query.answer(text="Done!")
+        await bot.send_message(
+            callback_query.from_user.id,
+            f"Done! You selected: {', '.join(checked_options)}",
+        )
+    else:
+        checkbox_states[selected_option] = not checkbox_states[selected_option]
+        await callback_query.message.delete()
+        await send_checkbox(callback_query.message)
 
 
 def main():
