0932d0234708176696fe7cbbc8df924a7ea1e1ce
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2024-01-31 23:37:11 +0200

    Implement correct behaviour between functions

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` command
     """
+    user_id = message.from_user.id
+    user_data[user_id] = dict()
     keyboard = types.ReplyKeyboardMarkup(
         resize_keyboard=True, one_time_keyboard=True
     )
@@ -39,38 +41,36 @@ async def send_welcome(message: types.Message):
 
 
 @dp.message_handler(lambda message: message.text and message.text in locations)
-async def save_location(message: types.Message):
+async def process_location(message: types.Message):
     """
     This handler will be called when user chose location
     """
     user_id = message.from_user.id
     location = message.text
-    user_data[user_id] = dict()
     user_data[user_id]["location"] = location
-    user_data[user_id]["checkbox_options"] = {
+    user_data[user_id]["checklist_options"] = {
         option: False for option in options
     }
     await message.reply(f"Твою локацію було збережено!")
-    await send_checkbox(user_id)
+    await send_checklist(user_id)
 
 
-async def send_checkbox(user_id: int):
+async def send_checklist(user_id: int):
     """
     This function will give user a check-list to select options
     """
     keyboard = types.InlineKeyboardMarkup()
 
-    for option, checked in user_data[user_id]["checkbox_options"].items():
+    for option, checked in user_data[user_id]["checklist_options"].items():
         text = f"✅ {option}" if checked else option
         button = types.InlineKeyboardButton(
-            text=text, callback_data=f"checkbox_{option}"
+            text=text, callback_data=f"checklist_{option}"
         )
         keyboard.add(button)
 
-    button = types.InlineKeyboardButton(
-        text="Send", callback_data="checkbox_Send"
+    keyboard.add(
+        types.InlineKeyboardButton(text="Send", callback_data="checklist_Send")
     )
-    keyboard.add(button)
     await bot.send_message(
         chat_id=user_id,
         text="Обери пункти, які були виконані:",
@@ -78,8 +78,8 @@ async def send_checkbox(user_id: int):
     )
 
 
-@dp.callback_query_handler(lambda query: query.data.startswith("checkbox_"))
-async def process_checkbox_callback(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(lambda query: query.data.startswith("checklist_"))
+async def process_checklist_callback(callback_query: types.CallbackQuery):
     """
     This handler will process user's choice from check-list
     """
@@ -87,29 +87,28 @@ async def process_checkbox_callback(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
 
     if selected_option == "Send":
-        checked_options = [
-            option
-            for option, checked in user_data[user_id][
-                "checkbox_options"
-            ].items()
-            if checked
-        ]
-        user_data[user_id]["checkbox_options"] = checked_options
+        checked_options = []
+
+        for option, checked in user_data[user_id]["checklist_options"].items():
+            if checked:
+                checked_options.append(option)
+
+        user_data[user_id]["checklist_options"] = checked_options
         await callback_query.answer(text="Готово!")
         await bot.send_message(
-            callback_query.from_user.id,
+            user_id,
             f"Готово! Ти обрав(ла) наступні пункти: {', '.join(checked_options)}.",
         )
-        await final_option(callback_query.message)
+        await final_option(user_id)
     else:
-        user_data[user_id]["checkbox_options"][
+        user_data[user_id]["checklist_options"][
             selected_option
-        ] = not user_data[user_id]["checkbox_options"][selected_option]
+        ] = not user_data[user_id]["checklist_options"][selected_option]
         await callback_query.message.delete()
-        await send_checkbox(user_id)
+        await send_checklist(user_id)
 
 
-async def final_option(message: types.Message):
+async def final_option(user_id: int):
     """
     This function will give user a final option to select before summary
     """
@@ -117,7 +116,8 @@ async def final_option(message: types.Message):
         resize_keyboard=True, one_time_keyboard=True
     )
     buttons = (types.KeyboardButton(text=option) for option in final_options)
-    await message.answer(
+    await bot.send_message(
+        chat_id=user_id,
         text="Обери фінальний пункт:",
         reply_markup=keyboard.add(*buttons),
     )
@@ -130,7 +130,6 @@ async def process_final_option(message: types.Message):
     """
     This handler will process user's final option
     """
-    user_id = message.from_user.id
     selected_final_option = message.text
 
     if selected_final_option == "Все чисто":
@@ -149,6 +148,13 @@ async def process_comment(message: types.Message):
     This function will process user's comment
     """
     user_id = message.from_user.id
+
+    if "location" not in user_data[user_id]:
+        await message.answer(
+            "Будь ласка, обери локацію написавши команду /start і вибравши її з пропонованого меню."
+        )
+        return
+
     user_data[user_id]["comment"] = message.text
     await message.answer(
         "Тепер можеш завантажити фото, якщо є "
@@ -163,33 +169,29 @@ async def process_photo(message: types.Message):
     """
     user_id = message.from_user.id
     photo = message.photo[-1]
-    user_data[user_id]["photo"] = photo
     file_path = await bot.get_file(photo.file_id)
     photo_name = f"./{user_id}_photo.jpg"
-    user_data[user_id]["photo_name"] = photo_name
     await bot.download_file(file_path.file_path, photo_name)
+    await get_report(message)
 
 
 @dp.message_handler(commands=["getreport"])
 async def get_report(message: types.Message):
     user_id = message.from_user.id
     location = user_data[user_id]["location"]
-    selected_checkbox_options = user_data[user_id]["checkbox_options"]
+    selected_checklist_options = user_data[user_id]["checklist_options"]
     comment = user_data[user_id].get("comment", None)
     photo_name = user_data[user_id].get("photo_name", None)
 
-    # with open(photo_name, "rb") as photo_file:   # TODO: send photo to openai
-    #     photo = photo_file
-
     user_data_for_report = (
         f"\nЛокація: {location}"
-        f"\nПункти, які було обрано в чеклисті: {', '.join(selected_checkbox_options)}"
+        f"\nПункти, які було обрано в чек листі: {', '.join(selected_checklist_options)}"
         f"\nКоментар: {comment}"
     )
     await message.answer(
-        f"Готово! Твій звіт буде створений на основі наступних даних: {user_data_for_report}"
+        f"Готово! Твій звіт буде створений на основі фото (якщо було додано) і наступних даних: {user_data_for_report}"
     )
-    report = make_report(user_data_for_report)
+    report = make_report(user_data_for_report, photo_name)
     await message.answer(report)
 
 
--- utils.py
+++ utils.py
@@ -10,56 +10,42 @@ OPENAI_KEY = os.getenv("OPENAI_KEY")
 client = OpenAI(api_key=OPENAI_KEY)
 
 
-def make_report(user_data):
+def make_report(user_data, photo_name=None):
+    base64_image = encode_image(photo_name) if photo_name else None
+    payload_user_content = [
+        {
+            "type": "text",
+            "text": f"Зроби звіт на основі наступних даних {user_data} і фото (якщо є):",
+        },
+    ]
+
+    if base64_image:
+        payload_user_content.append(
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+            },
+        )
+
     completion = client.chat.completions.create(
+        # model="gpt-4-vision-preview",   # TODO: change to gpt4
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": "Ти асистент, який робить звіт на основі відповідей користувача. "
-                "Не важливо яка тема і які будуть вхідні дані від користувача, "
-                "ти у будь якому випадку маєш відправити звіт!",
+                "content": "Ти асистент, який робить професійний звіт на основі відповідей користувача "
+                "і відправляю цьому ж користувачу цей звіт. Не важливо які будуть вхідні дані від користувача, "
+                "ти у будь якому випадку маєш відправити сформований звіт на приблизно 50-100 слів!",
             },
             {
                 "role": "user",
-                "content": f"Зроби звіт (50-100 слів) на основі наступних даних: {user_data}",
+                "content": payload_user_content,
             },
         ],
     )
     return completion.choices[0].message.content
 
 
-# def encode_image(image_path):
-#     with open(image_path, "rb") as image_file:
-#         return base64.b64encode(image_file.read()).decode("utf-8")
-#
-#
-# base64_image = encode_image("398513306_photo.jpg")
-
-
-# def make_report(user_data):
-#     completion = client.chat.completions.create(
-#         model="gpt-4-vision-preview",
-#         messages=[
-#             {
-#                 "role": "system",
-#                 "content": "Ти асистент, який робить звіт на основі відповідей користувача. "
-#                 "Не важливо яка тема і які будуть вхідні дані від користувача, "
-#                 "ти у будь якому випадку маєш відправити звіт!",
-#             },
-#             {
-#                 "role": "user",
-#                 "content": [
-#                     {"type": "text", "text": "What’s in this image?"},
-#                     {
-#                         "type": "image_url",
-#                         "image_url": {
-#                             "url": f"data:image/jpeg;base64,{base64_image}"
-#                         },
-#                     },
-#                 ],
-#             },
-#         ],
-#     )
-#     # return completion.choices[0].message.content
-#     return completion
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
