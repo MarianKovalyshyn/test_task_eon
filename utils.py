a8350f68bda0aa4beb2bfa0f8569530482f9686e
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2024-02-01 17:56:24 +0200

    Add README.md

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def make_report(user_data, photo_name=None):
         )
 
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4-vision-preview",
         messages=[
             {
                 "role": "system",
